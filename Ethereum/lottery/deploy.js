f41901ddb015adda3dfc914c23f1c117aeaa2b45
MatthewVella98 <matthewvella658@gmail.com>
2019-10-28 15:40:08 +0100

    Init commit
    Initial commit

---
+++ Ethereum/lottery/deploy.js
@@ -0,0 +1,22 @@
+const HDWalletProvider = require('truffle-hdwallet-provider');
+const Web3 = require('web3');
+const compiled = require('./compile');
+
+const provider = new HDWalletProvider(
+    'mammal waste glare genius praise unaware will fiction add corn hobby lens',
+    'https://rinkeby.infura.io/v3/ccf57dddf77c40cdac915939d73fc21c'
+);
+const web3 = new Web3(provider);
+
+const deploy = async () => {
+  const accounts = await web3.eth.getAccounts();
+  console.log(accounts[0]);
+  console.log('Attempting to deploy from account', accounts[0]);
+ 
+  const result = await new web3.eth.Contract(JSON.parse(compiled.interface))
+    .deploy({ data: compiled.bytecode })
+    .send({ gas: '1000000', from: accounts[0] });
+
+  console.log('Contract deployed to', result.options.address);
+};
+deploy();
